54b4edb238737987c642987d65d26d9998fafc6e
Aleleonel <aleleonel@gmail.com>
2021-02-02 17:20:49 -0300

    cadastro de movimentações

--- Projects/LojaCaixa001/UfrmCadMovimentacao.pas
+++ Projects/LojaCaixa001/UfrmCadMovimentacao.pas
@@ -4,10 +4,23 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.DBCtrls, Vcl.Mask,
+  Vcl.ExtCtrls, Data.DB, Vcl.Grids, Vcl.DBGrids;
 
 type
   TfrmCadMovimentacao = class(TForm)
+    Label1: TLabel;
+    DBComboBox1: TDBComboBox;
+    DBEdit1: TDBEdit;
+    DBEdit2: TDBEdit;
+    DBMemo1: TDBMemo;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    DBNavigator1: TDBNavigator;
+    DBGrid1: TDBGrid;
+    procedure DBNavigator1Click(Sender: TObject; Button: TNavigateBtn);
   private
     { Private declarations }
   public
@@ -20,5 +33,16 @@ var
 implementation
 
 {$R *.dfm}
+
+uses UDM;
+
+procedure TfrmCadMovimentacao.DBNavigator1Click(Sender: TObject;
+  Button: TNavigateBtn);
+begin
+   if Button = nbInsert then
+     begin
+         DM.tbMovimentacoes.FieldByName('dataHora').Value := Now;
+     end;
+end;
 
 end.
